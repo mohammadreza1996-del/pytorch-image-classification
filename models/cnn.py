66d494660702934f330d07f9fac4403da00cf851
mohammadreza <mohammadreza.mohammadii1996@gmail.com>
2026-02-18 11:27:15 -0800

    add CNN model

--- models/cnn.py
+++ models/cnn.py
@@ -1,1 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class CNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64*8*8, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64*8*8)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
