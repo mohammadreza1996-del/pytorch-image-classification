d4d2c7142019ab62fc9a8244ce9b2b169d74f021
mohammadreza <mohammadreza.mohammadii1996@gmail.com>
2026-02-18 11:33:25 -0800

    Update train.py

--- models/train.py
+++ models/train.py
@@ -1,1 +1,50 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from models.cnn import CNN
+import torch.nn as nn
+import torch.optim as optim
 
+# Device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Transformations
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+])
+
+# Dataset
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = DataLoader(testset, batch_size=64, shuffle=False)
+
+# Model
+model = CNN(num_classes=10).to(device)
+
+# Loss & optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+for epoch in range(5):  # 5 epochs for example
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        if i % 100 == 99:    # print every 100 mini-batches
+            print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 100:.3f}")
+            running_loss = 0.0
+
+print("Finished Training")
